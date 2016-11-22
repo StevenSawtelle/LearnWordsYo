25ea065f31c3758f999fc9c13fc2cbbd97f07a65
StevenSawtelle <steven.sawtelle@gmail.com>
2016-11-21 22:51:20 -0700

    adding example config.js
    config.js holds your login credentials in OAuth, use them from your twitter dev page to log in

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	consumer_key: '',
+	consumer_secret: '',
+	access_token: '',
+  access_token_secret:  '',
+	timeout_ms: ''
+}
+//this file is for the login credentials of your twitter account. Fill it out with private tokens and keys
